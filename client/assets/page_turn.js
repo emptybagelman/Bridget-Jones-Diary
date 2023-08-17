15f92bd6284354da0daf7a39eb7a3c7e646fdccf
emptybagelman <samuelmcgin@aol.com>
2023-08-17 13:48:36 +0100

    page styling

--- client/assets/page_turn.js
+++
@@ -1,9 +0,0 @@
-const book = document.querySelector(".book");
-
-book.addEventListener("click", () => {
-    book.classList.toggle("turnPageAnim");
-})
-
-book.addEventListener("animationend", () => {
-    book.classList.toggle("turnPageAnim");
-})
